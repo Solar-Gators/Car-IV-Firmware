ab525c88104e1dbf923cb46ec29e7e32aaca1925
Matthew Shen <shenmatthew@outlook.com>
2023-09-04 18:18:11 -0400

    Added Github Actions workflow

--- Resources/Drivers/SG_Drivers/Src/bq76952.cpp
+++
@@ -1,7 +0,0 @@
-#include "bq76952.hpp"
-
-void BQ76952::SendData(uint8_t register_address, uint8_t *data) {
-    uint16_t devAddress = (I2C_RES_ADDR << 8) | register_address;
-
-    HAL_I2C_Master_Transmit(phi2c_, devAddress, data, 1, HAL_MAX_DELAY);
-}
