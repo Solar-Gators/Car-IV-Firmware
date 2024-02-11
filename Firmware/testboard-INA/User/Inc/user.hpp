2e7e060b042e13e71b58889d4e7f341c90f1488e
jackson-werner <112117237+jackson-werner@users.noreply.github.com>
2024-02-11 10:07:07 -0500

    Fixed arithmatic for receiving bytes from the INA226 (#8)
    * Fixed byte reading order for read buffer, changed uint to int so negative logic isnt manual
    
    * Fixed float math to correct current_LSB_, shunt voltage, current, and bus voltage all are functional now

---
+++ Firmware/testboard-INA/User/Inc/user.hpp
@@ -0,0 +1,14 @@
+#ifndef USER_HPP_
+#define USER_HPP_
+
+#include <stddef.h>
+
+#include "main.h"
+
+#include "cmsis_os.h"
+#include "logger.hpp"
+#include "INA226.hpp"
+
+extern "C" I2C_HandleTypeDef hi2c2;
+
+#endif /* USER_HPP_ */
---
+++ Firmware/testboard-INA/User/Src/user.cpp
@@ -0,0 +1,29 @@
+#include "user.hpp"
+
+extern "C" void CPP_UserSetup(void);
+
+void CPP_UserSetup(void) {
+    INA226 INA;
+
+    HAL_StatusTypeDef status;
+    status = INA.Init(&hi2c2);
+    if(status != HAL_OK) while(1);
+
+    INA.SetConfig();
+
+    INA.SetCalibrationReg(0.02, 5.0);
+
+    while(1){
+        status = INA.GetShuntVoltage();
+        if(status != HAL_OK) while(1);
+        HAL_Delay(500);
+
+				status = INA.GetBusVoltage();
+				if(status != HAL_OK) while(1);
+        HAL_Delay(500);
+
+        status = INA.GetCurrent();
+        if(status != HAL_OK) while(1);
+        HAL_Delay(500);
+    }
+}
