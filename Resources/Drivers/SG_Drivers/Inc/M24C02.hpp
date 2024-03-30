d0bfec90f359143b904eb914732510af2736c362
Joshua Kwak <joshua.paul.kwak@gmail.com>
2024-03-30 17:40:28 -0400

    cool

--- Resources/Drivers/SG_Drivers/Inc/M24C02.hpp
+++ Resources/Drivers/SG_Drivers/Inc/M24C02.hpp
@@ -4,9 +4,6 @@
 #define M24C02_HPP_
 
 #include "stm32l4xx_hal.h"
-#include <cstring>
-#include <cstdint>
-#include <vector>
 
 using namespace std;
 
@@ -14,99 +11,12 @@ using namespace std;
 
 #define M24C02_I2C_ADDR (0b10100000)
 
-class memory{ //Memory class containing ID, address, data type (i,f,c,b), and a name.
 
-    
-    public:
-
-        //Defines getter functions
-        int getID(){
-            return ID;
-        }
-
-        uint8_t getAddr(){
-            return Addr;
-        }
-
-        uint8_t getSize(){
-            return size;
-        }
-
-        const char* getName(){
-            return name;
-        }
-
-        char getType(){
-            return type;
-        }
-
-        //Defines constructor
-        //Supported data types: int (i), float (f), char (c)
-        memory(int ID, uint8_t Addr, char type, const char* name){
-            ID = ID;
-            Addr = Addr;
-            type = type;
-            name = name;
-            switch (type){
-
-                case 'i':
-                case 'f':
-                size = 4;
-                break;
-
-                case 'c':
-                case 'b':
-                size = 1;
-                break;
-
-                default:
-                size = 1;
-                break;
-            }
-
-
-        }
-        
-        memory(){
-            ID = -1;
-            Addr = 0x00;
-            type = 'n';
-            name = "null";
-            size = 0;
-
-        }
-
-        ~memory(){
-            delete name;
-        }
-
-    private:
-        int ID;
-        uint8_t Addr;
-        int size;
-        const char* name;
-        char type;
-};
-
-//Include all memory objects below in the array.
-/*memory storage[] = { //Defines an array to store all of the memory objects in.
-
-    //The IDs of every object should be the number of the object in the array. i.e. memory[0] == ID = 0
-    memory(0, 0x00, 'f', "Potential"),
-    memory(1, 0x03, 'f', "Integral"),
-    memory(2, 0x08, 'f', "Derivative"),
-    memory(3, 0x0C, 'f', "Odometer"),
-    memory(4, 0x10, 'f', "Regen"),
-    memory(5, 0x14, 'f', "Speed")
-};*/
-//All address values are in hex.
-//Floats and Ints are 4 bytes, Chars and Bools are 1 byte.
+class M24C02{
 
 
 
-typedef struct {
-    I2C_HandleTypeDef *i2cHandle;
-} M24C02;
+}
 
 struct {
 
@@ -121,12 +31,10 @@ HAL_StatusTypeDef M24C02_UpdateOne(M24C02 *dev, int ID, uint8_t newVal);
 HAL_StatusTypeDef M24C02_TickOdometer(M24C02 *dev);
 
 //Low level functions
-//Utilize overloading so only one function is needed
-// HAL_StatusTypeDef M24C02_ReadRegister(M24C02 *dev, uint8_t reg, uint8_t data);
-HAL_StatusTypeDef M24C02_ReadRegister(M24C02 *dev, uint8_t reg, uint8_t *data, uint8_t length);
 
-HAL_StatusTypeDef M24C02_WriteRegister(M24C02 *dev, uint8_t reg, uint8_t *data);
-HAL_StatusTypeDef M24C02_WriteRegister(M24C02 *dev, uint8_t reg, uint8_t *data, uint8_t length);
+HAL_StatusTypeDef M24C02_ReadRegister(M24C02 *dev, uint8_t reg, uint8_t *data, int length = 1);
+
+HAL_StatusTypeDef M24C02_WriteRegister(M24C02 *dev, uint8_t reg, uint8_t *data, int length = 1);
 
 #endif /* USER_HPP_ */
 
--- Resources/Drivers/SG_Drivers/Src/M24C02.cpp
+++ Resources/Drivers/SG_Drivers/Src/M24C02.cpp
@@ -171,17 +171,10 @@ HAL_StatusTypeDef M24C02_INIT(M24C02 *dev, I2C_HandleTypeDef *i2cHandle){
 	return HAL_OK;
 }
 
-// HAL_StatusTypeDef M24C02_ReadRegister(M24C02 *dev, uint8_t reg, uint8_t *data){ //Function to read data off the EEPROM.
-//     return HAL_I2C_Mem_Read(dev->i2cHandle, M24C02_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
-// }
-
-HAL_StatusTypeDef M24C02_ReadRegister(M24C02 *dev, uint8_t reg, uint8_t *data, uint8_t length){ //Function to read data off the EEPROM. Utilizes overloading.
+HAL_StatusTypeDef M24C02_ReadRegister(M24C02 *dev, uint8_t reg, uint8_t *data, int length = 1){ //Function to read data off the EEPROM.
     return HAL_I2C_Mem_Read(dev->i2cHandle, M24C02_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, length, HAL_MAX_DELAY);
 }
 
-HAL_StatusTypeDef M24C02_WriteRegister(M24C02 *dev, uint8_t reg, uint8_t *data){ //Function to write data to the EEPROM.
-	return HAL_I2C_Mem_Write(dev->i2cHandle, M24C02_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
-}
-HAL_StatusTypeDef M24C02_WriteRegister(M24C02 *dev, uint8_t reg, uint8_t *data, uint8_t length){ //Function to write data to the EEPROM. Utilizes overloading.
+HAL_StatusTypeDef M24C02_WriteRegister(M24C02 *dev, uint8_t reg, uint8_t *data, uint8_t length = 1){ //Function to write data to the EEPROM.
 	return HAL_I2C_Mem_Write(dev->i2cHandle, M24C02_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, length, HAL_MAX_DELAY);
 }
