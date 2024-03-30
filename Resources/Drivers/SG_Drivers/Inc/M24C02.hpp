f41bce63806239589d5422e8abd6072e2caca221
ASattar04 <sattarahmed204@gmail.com>
2024-03-30 18:04:28 -0400

    Started M24C02 class

--- Resources/Drivers/SG_Drivers/Inc/M24C02.hpp
+++ Resources/Drivers/SG_Drivers/Inc/M24C02.hpp
@@ -14,27 +14,35 @@ using namespace std;
 
 class M24C02{
 
+public:
+
+    M24C02();
+
+    HAL_StatusTypeDef M24C02_INIT(M24C02 *dev, I2C_HandleTypeDef *i2cHandle);
+    HAL_StatusTypeDef M24C02_ReadAll(M24C02 *dev, float *data);
+    HAL_StatusTypeDef M24C02_UpdateOne(M24C02 *dev, int ID, uint8_t newVal);
+    HAL_StatusTypeDef M24C02_TickOdometer(M24C02 *dev);
+    
+    HAL_StatusTypeDef M24C02_ReadRegister(M24C02 *dev, uint8_t reg, uint8_t *data, int length = 1);
+
+    HAL_StatusTypeDef M24C02_WriteRegister(M24C02 *dev, uint8_t reg, uint8_t *data, int length = 1);
 
 
-}
+};
 
 struct {
+
 
 
 
 } typedef memory;
 
 //High level functions
-HAL_StatusTypeDef M24C02_INIT(M24C02 *dev, I2C_HandleTypeDef *i2cHandle);
-HAL_StatusTypeDef M24C02_ReadAll(M24C02 *dev, float *data);
-HAL_StatusTypeDef M24C02_UpdateOne(M24C02 *dev, int ID, uint8_t newVal);
-HAL_StatusTypeDef M24C02_TickOdometer(M24C02 *dev);
+
 
 //Low level functions
 
-HAL_StatusTypeDef M24C02_ReadRegister(M24C02 *dev, uint8_t reg, uint8_t *data, int length = 1);
 
-HAL_StatusTypeDef M24C02_WriteRegister(M24C02 *dev, uint8_t reg, uint8_t *data, int length = 1);
 
 #endif /* USER_HPP_ */
 
