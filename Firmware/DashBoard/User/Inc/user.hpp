4f29df95e684327ea67a159de898b173bc270e3b
Matthew Shen <shenmatthew@outlook.com>
2024-05-05 17:36:15 -0400

    Fixed dashboard adc

---
+++ Firmware/DashBoard/User/Inc/user.hpp
@@ -0,0 +1,24 @@
+#ifndef USER_HPP_
+#define USER_HPP_
+
+#include <stddef.h>
+
+#include "main.h"
+
+#include "cmsis_os.h"
+#include "logger.hpp"
+// #include "usbd_cdc_if.h"
+
+#include "sg_can.hpp"
+#include "ADS7138.hpp"
+
+/* Datamodules */
+#include "DriverControls.hpp"
+#include "MotorControlFrame.hpp"
+
+extern "C" I2C_HandleTypeDef hi2c2;
+extern "C" ADC_HandleTypeDef hadc2;
+
+
+
+#endif /* USER_HPP_ */
---
+++ Firmware/DashBoard/User/Src/user.cpp
@@ -0,0 +1,175 @@
+#include "user.hpp"
+#include "IMU.h"
+// #include "usbd_cdc_if.h"
+#include <string>
+#include "IoTestFrame.hpp"
+#include "MotorControlFrame.hpp"
+#include "ADS7138.hpp"
+
+using namespace std;
+
+/* In this program, two tasks are generated. One is triggered by a periodic timer, which
+ * expires every 500ms. The other is a regular thread, which is triggered by and event flag.
+ * The periodic timer task sets the event flag, which causes the regular thread to exit
+ * its wait state and execute. The regular thread then toggles the LED and prints a message.
+ * 
+ * CPP_UserSetup is called from main.cpp, and is where the user should put their setup code.
+ * It is run before the RTOS scheduler is started.
+*/
+extern "C" CAN_HandleTypeDef hcan1;
+extern "C" CAN_HandleTypeDef hcan2;
+extern "C" I2C_HandleTypeDef hi2c2;
+
+/* Initialize CAN frames and devices */
+
+CANDevice candev1 = CANDevice(&hcan1);
+CANDevice candev2 = CANDevice(&hcan2);
+
+
+ADS7138 adcs[1] = {ADS7138(&hi2c2, 0x10)};
+
+
+extern "C" void CPP_UserSetup(void);
+
+/* Task function prototypes */
+void PeriodicTask1(void *argument);
+void RegularTask1(void *argument);
+
+/* Periodic timer definitions */
+osTimerAttr_t periodic_timer_attr = {
+    .name = "Periodic Task 1",
+    .attr_bits = 0,
+    .cb_mem = NULL,
+    .cb_size = 0,
+};
+osTimerId_t periodic_timer_id = osTimerNew((osThreadFunc_t)PeriodicTask1, osTimerPeriodic, NULL, &periodic_timer_attr);
+
+/* Regular task definitions */
+osThreadId_t regular_task_id;
+uint32_t regular_task_buffer[128];
+StaticTask_t regular_task_control_block;
+const osThreadAttr_t regular_task_attributes = {
+    .name = "Regular Task 1",
+    .attr_bits = osThreadDetached,
+    .cb_mem = &regular_task_control_block,
+    .cb_size = sizeof(regular_task_control_block),
+    .stack_mem = &regular_task_buffer[0],
+    .stack_size = sizeof(regular_task_buffer),
+    .priority = (osPriority_t) osPriorityAboveNormal,
+    .tz_module = 0,
+    .reserved = 0,
+};
+
+/* Event flag to trigger regular task */
+osEventFlagsId_t regular_event = osEventFlagsNew(NULL);
+
+
+uint16_t rawData;
+uint8_t high;
+uint8_t low;
+uint8_t ignitionState = false;
+void CAN_Modules_Init() {
+    // Add CAN devices and CAN frames
+    CANController::AddDevice(&candev1);
+    CANController::AddDevice(&candev2);
+    CANController::AddFilterAll();
+    CANController::Start();
+}
+void ADC_Modules_Init() {
+   
+    
+    if (adcs[0].Init() != HAL_OK || adcs[0].TestI2C() != HAL_OK)
+        Logger::LogError("ADC %d init failed", 0);
+    else
+        Logger::LogInfo("ADC %d init success", 0);
+
+    // Configure all channels as ADC inputs
+    if (adcs[0].ConfigurePinMode(0x0) != HAL_OK)
+        Logger::LogError("ADC %d pin mode configure failed", 0);
+    
+    // Configure auto-sequence
+    if (adcs[0].ConfigureSequenceMode(SeqMode_Type::AUTO) != HAL_OK)
+        Logger::LogError("ADC %d sequence mode configure failed", 0);
+
+    // Configure sampling rate to 2.6 kSPS
+    if (adcs[0].ConfigureOpmode(false, 
+                                ConvMode_Type::AUTONOMOUS, 
+                                Osc_Type::LOW_POWER, 
+                                0b0111) != HAL_OK)
+        Logger::LogError("ADC %d opmode configure failed", 0);
+
+    // Configure oversampling to 128x
+    if (adcs[0].ConfigureOversampling(OsrCfg_Type::OSR_128) != HAL_OK)
+        Logger::LogError("ADC %d oversampling configure failed", 0);
+
+    // Enable statistics
+    if (adcs[0].ConfigureStatistics(true) != HAL_OK)
+        Logger::LogError("ADC %d statistics enable failed", 0);
+
+    // Append channel ID to data
+    if (adcs[0].ConfigureData(false, DataCfg_AppendType::ID) != HAL_OK)
+        Logger::LogError("ADC %d channel ID append failed", 0);
+
+    // Sequence channels 0 and 1
+    if (adcs[0].ConfigureSequence(0x3) != HAL_OK)
+        Logger::LogError("ADC %d sequence channels configure failed", 0);
+
+    // Start sequencing
+    if (adcs[0].StartSequence() != HAL_OK)
+        Logger::LogError("ADC %d start failed", 0);
+}
+
+void CPP_UserSetup(void) {
+    // Make sure that timer priorities are configured correctly
+    HAL_Delay(10);
+    
+    // CAN_Modules_Init();
+
+
+    CANController::AddDevice(&candev1);
+    CANController::AddDevice(&candev2);
+    //CANController::AddRxMessage(&io_test_frame, IoMsgCallback);
+    CANController::AddFilterAll();
+    CANController::Start();
+
+    ADC_Modules_Init();
+
+    regular_task_id = osThreadNew((osThreadFunc_t)RegularTask1, NULL, &regular_task_attributes);
+
+    // 20Hz periodic timer
+    osTimerStart(periodic_timer_id, 50);
+}
+
+static uint8_t counter = 0;
+void PeriodicTask1(void *argument) {
+    //heart beat
+    if (counter++ % 10 == 0)
+        HAL_GPIO_TogglePin(OK_LED_GPIO_Port, OK_LED_Pin);
+
+    if (adcs[0].ReadChannel(0, &rawData) != HAL_OK)
+        Logger::LogError("ADC %d read failed", 0);
+
+    DriverControlsFrame0::SetThrottleVal((uint16_t)(rawData) << 4);
+    
+    CANController::Send(&DriverControlsFrame0::Instance());
+
+    osEventFlagsSet(regular_event, 0x1);
+    
+    if(HAL_GPIO_ReadPin(GPIOC, 6) == true){
+       DriverControlsFrame0::SetShutdownStatus((true));
+    }
+
+
+}
+
+void RegularTask1(void *argument) {
+    while (1) {
+        osEventFlagsWait(regular_event, 0x1, osFlagsWaitAny, osWaitForever);
+
+       // Logger::LogInfo("Hello World!\n");
+        
+        
+    }
+}
+
+
